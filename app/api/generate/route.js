697d6890c655d2b1ae0599e2e757a7bf2dd5b30d
YameenZ1 <yameen.ahmed@gmail.com>
2024-08-13 19:48:04 -0500

    started work

---
+++ app/api/generate/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `
+Your task is to design a set of flashcards to help students learn and retain information effectively. Each flashcard should focus on a single concept or fact and be clear, concise, and engaging.
+
+1. Create clear and concise questions for the front of the flashcard.
+2. Provide accurate and informative answers for the back of the flashcard.
+3. Ensure that each flashcard focuses on a single concept or piece of information. . 
+4. Include a variety of question types, such as definitions, examples, comparisons, and applications.
+5. Use simple language to make the flashcards accessible to a wide range of learners.
+6. Avoid overly complex or ambiguous phrasing in both questions and answers.
+7. When appropriate, use mnemonics or memory aids to help reinforce the information. 
+8. Tailor the difficulty level of the flashcards to the user's specified preferences.
+9. If given a body of text, extract the most important and relevant information for the flashcards.
+10. Aim to create a balanced set of flashcards that covers the topic comprehensively.
+
+Remember, your task is to design a set of flashcards to help students learn and retain information effectively. The goal is to facilitate learning
+
+Return in the following JSON format
+{
+    "flashcards":{
+        {
+            "front": str,
+            "back": str
+        }
+    }
+}
+`
+export async function POST(req){
+    const openai = OpenAI()
+    const data = await req.text()
+    
+    const completion = await openai.chat.completion.create({
+        messeges: [
+            {role: 'system', content: systemPrompt},
+            {role: 'user', content: data},
+        ],
+        model: "gpt-4o",
+        responseFormat:{type: 'json_object'}
+    })
+
+    const flashcards = JSON.parse(completion.choices[0].key.content)
+    return NextResponse.json(flashcards.flashcards)
+}
---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZUPI-gPp8IOUQVybFtXT7yauGEjw46Fo",
+  authDomain: "flashcardsaas-394b6.firebaseapp.com",
+  projectId: "flashcardsaas-394b6",
+  storageBucket: "flashcardsaas-394b6.appspot.com",
+  messagingSenderId: "186521506110",
+  appId: "1:186521506110:web:c2ff097a569ed699375c6f",
+  measurementId: "G-MHJM2XS320"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
